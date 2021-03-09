74186490a6c8c4a427ddaf307d7b311320dd8da5
RizhiyAnanas <fou081096@gmail.com>
2021-03-09 16:12:49 +0300

    2.2.1

---
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -0,0 +1,35 @@
+package hiber.service;
+
+import hiber.dao.UserDao;
+import hiber.dao.UserDaoImp;
+import hiber.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+
+   @Autowired
+   private UserDao userDao;
+
+   @Transactional
+   @Override
+   public void add(User user) {
+      userDao.add(user);
+   }
+
+   @Transactional(readOnly = true)
+   @Override
+   public List<User> listUsers() {
+      return userDao.listUsers();
+   }
+
+   @Override
+   public User getBySeriesAndModel(int series, String model) {
+      return userDao.getBySeriesAndModel(series, model);
+   }
+
+}
